541974102f7838cef7406b04ca9798bfbb5deec8
Debangshi Pramanik <debangshipramanik84@gmail.com>
2025-09-03 15:29:29 +0530

    Add files via upload

---
+++ tower_of_hanoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void hanoi(int n, char source, char target, char auxiliary) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, target);
+        return;
+    }
+    hanoi(n - 1, source, auxiliary, target);
+    printf("Move disk %d from %c to %c\n", n, source, target);
+    hanoi(n - 1, auxiliary, target, source);
+}
+
+int main() {
+    int n;
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
+}
